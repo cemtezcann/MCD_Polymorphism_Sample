ec874fc2959e1d9a17ba0db045a129a42a3cb69e
cmtzc <tezcancem92@gmail.com>
2021-12-06 16:26:14 +0300

    Add project files.

---
+++ MCD_Polymorphism_Sample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_Polymorphism_Sample
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //polymorphism
+
+            Console.WriteLine("Banka seçiniz A / B :");
+            ConsoleKeyInfo secim;
+            secim = Console.ReadKey();
+
+
+            if (secim.Key == ConsoleKey.A)
+            {
+                ABankasi bankam = new ABankasi();
+                bankam.BilgileriAl();
+                bankam.IndirimOraninaGoreIndirimYap();
+            }
+            else if (secim.Key == ConsoleKey.B)
+            {
+                BBankasi bankam = new BBankasi();
+                bankam.BilgileriAl();
+                bankam.IndirimOraninaGoreIndirimYap();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
